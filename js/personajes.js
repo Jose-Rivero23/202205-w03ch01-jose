51dfc58467b4cbac5c1c9977e710ae1fe44a3740
Jose Rivero <josearmandorr95@gmail.com>
2022-05-23 20:37:39 +0200

    =AddTemplates

--- js/personajes.js
+++ js/personajes.js
@@ -7,14 +7,9 @@ import { Rey } from './Rey.js';
 let p1 = new Rey('Joffrey', 'Baratheon', '14', '1');
 let p2 = new Luchador('Jaime', 'Lannister', '43', 'Espada', 8);
 let p3 = new Luchador('Daenerys', 'Targaryen', '24', 'Dragons', 10);
-let p4 = new Asesor('Tyrion', 'Lannister', '39', 'Daenerys');
-let p5 = new Escudero('Bronn', '', '32', 'Jaime', 5);
+let p4 = new Asesor('Tyrion', 'Lannister', '39', p3.name);
+let p5 = new Escudero('Bronn', '', '32', p2.name, 5);
 
-export function render() {
-    GoT = [p1, p2, p3, p4, p5];
-
-    return GoT;
-}
-render();
+export const GoT = [p1, p2, p3, p4, p5];
 
 console.log(GoT);
